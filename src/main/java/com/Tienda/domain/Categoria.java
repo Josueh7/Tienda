5de0ba4e136074b071d1cfb546a653206db89bae
Josueh7 <josueh070499@gmail.com>
2023-11-06 14:41:28 -0600

    Semana 8

--- src/main/java/com/Tienda/domain/Categoria.java
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -1,7 +1,9 @@
 package com.Tienda.domain;
 
+
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data //Para la creacion automatica de los set y get de los atributos.
@@ -18,6 +20,10 @@ public class Categoria implements Serializable{
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    List <Producto> productos;
 
     public Categoria() {}
 
